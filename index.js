ff0403756b2f27079fdb54d857dba7e5512a8a8d
Alberto Labrada <albertolabrada99@gmail.com>
2022-02-10 17:32:21 -0600

    added auth with jwt

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 import { PostgrestClient } from '@supabase/postgrest-js'
 import { Router } from 'itty-router'
+import * as jwt from 'jsonwebtoken'
 
 const client = new PostgrestClient(POSTGREST_ENDPOINT, {
   fetch: (...args) => fetch(...args),
 })
 const router = Router({ base: '/api' })
+
+// Middleware for route protection
+const requireUser = (request) => {
+  const { query } = request
+  const token = query.accessToken
+  if (!token) {
+    return new Response('Not Authenticated', { status: 401 })
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET)
+
+    if (!decoded.user) {
+      return new Response('Invalid token', { status: 401 })
+    }
+  } catch (error) {
+    return new Response('Error verifying token', { status: 500 })
+  }
+}
 
 // @route   GET api/stocks
 // @desc    Get a list of all stocks
@@ -41,7 +61,7 @@ router.get('/stocks/:id', async ({ params }) => {
 // @route   POST api/stocks
 // @desc    Add a new stock
 // @access  Private
-router.post('/stocks', async (request) => {
+router.post('/stocks', requireUser, async (request) => {
   const stockData = await request.json()
 
   const { data: stock, error } = await client
@@ -56,10 +76,42 @@ router.post('/stocks', async (request) => {
 })
 
 
+// @route   POST api/auth/login
+// @desc    Logs in user
+// @access  Public
+router.post('/auth/login', async (request) => {
+  const { username, password } = await request.json()
+
+  const { data, error } = await client
+    .from('users')
+    .select()
+    .eq('username', username)
+    .eq('pass', password)
+
+  if (error) throw error
+
+  const user = data.length ? data[0] : null
+
+  if (!user) {
+    return new Response("User Not Found", { status: 401 })
+  }
+
+  const payload = {
+    user: {
+      id: user.id,
+    },
+  }
+
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: 360000 })
+  return new Response(JSON.stringify({ token }), { status: 200 })
+});
+
+
 // @route   *
 // @desc    Any other route will return Not Found with 404 Status
 router.all('*', () => new Response("Not Found", { status: 404 }))
 
 addEventListener('fetch', event => {
+  console.log('headers', JSON.stringify(event.request.headers));
   event.respondWith(router.handle(event.request))
 })
