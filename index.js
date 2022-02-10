d25de67e981df9b471fd500278b3fbe5b6d84648
Alberto Labrada <albertolabrada99@gmail.com>
2022-02-10 14:36:23 -0600

    setting up router and simple routes

--- index.js
+++ index.js
@@ -1,23 +1,65 @@
 import { PostgrestClient } from '@supabase/postgrest-js'
+import { Router } from 'itty-router'
+
+const client = new PostgrestClient(POSTGREST_ENDPOINT, {
+  fetch: (...args) => fetch(...args),
+})
+const router = Router({ base: '/api' })
+
+// @route   GET api/stocks
+// @desc    Get a list of all stocks
+// @access  Public
+router.get('/stocks', async () => {
+  const { data, error } = await client.from('stocks').select()
+  if (error) throw error
+
+  return new Response(JSON.stringify({ stocks: data }), {
+      headers: { 'content-type': 'application/json' },
+  })
+})
+
+// @route   GET api/stocks/:id
+// @desc    Get a the stock by id
+// @access  Public
+router.get('/stocks/:id', async ({ params }) => {
+  const { id } = params
+  const { data, error } = await client
+    .from('stocks')
+    .select()
+    .eq('id', id)
+
+  if (error) throw error
+
+  const stock = data.length ? data[0] : null
+
+  return new Response(JSON.stringify({ stock }), {
+      headers: { 'content-type': 'application/json' },
+      status: stock ? 200 : 404
+  })
+})
+
+// @route   POST api/stocks
+// @desc    Add a new stock
+// @access  Private
+router.post('/stocks', async (request) => {
+  const stockData = await request.json()
+
+  const { data: stock, error } = await client
+    .from('stocks')
+    .insert([stockData])
+
+  if (error) throw error
+
+  return new Response(JSON.stringify({ stock }), {
+    headers: { 'content-type': 'application/json' },
+  })
+})
+
+
+// @route   *
+// @desc    Any other route will return Not Found with 404 Status
+router.all('*', () => new Response("Not Found", { status: 404 }))
 
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(router.handle(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  const client = new PostgrestClient(POSTGREST_ENDPOINT, {
-    fetch: (...args) => fetch(...args),
-  })
-  const { data, error } = await client
-    .from('users')
-    .select()
-
-  return new Response(JSON.stringify(data), {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-}
